b8bc3a7353280e43e14640127af83d0f4da49c08
carolinadias25 <carolina.f.dias@hotmail.com>
2021-06-02 22:32:24 +0100

    first commit

--- scripts/create-database.js
+++ scripts/create-database.js
@@ -1,28 +1,46 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const path = require('path');
 
 const args = process.argv.slice(2)[0];
 
 const envFile = args === 'test' ? '../.env.test' : '../.env';
 
+console.log(`Using ${envFile}`);
+
 require('dotenv').config({
   path: path.join(__dirname, envFile),
 });
 
 const { DB_PASSWORD, DB_NAME, DB_USER, DB_HOST, DB_PORT } = process.env;
+console.log(`Using ${DB_NAME}`);
 
-const connection = mysql.createConnection({
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  port: DB_PORT,
-});
+const setUpDatabase = async () => {
+  try {
 
-connection.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`, (err) => {
-  if (err) {
-    console.log('Your environment variables might be wrong. Please double check .env file');
-    console.log('Environment Variables are:', { DB_PASSWORD, DB_NAME, DB_USER, DB_HOST, DB_PORT });
+    const db = await mysql.createConnection({
+      host: DB_HOST,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      port: DB_PORT,
+    });
+
+
+    await db.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`);
+    db.close();
+  } catch (err) {
+
+    console.log(
+      `Your environment variables might be wrong. Please double check .env file`
+    );
+    console.log('Environment Variables are:', {
+      DB_PASSWORD,
+      DB_NAME,
+      DB_USER,
+      DB_HOST,
+      DB_PORT,
+    });
     console.log(err);
   }
-  connection.close();
-});
+};
+
+setUpDatabase();
